d93a4047caeb4b7c1d37540a7c0d7a65f6e69048
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-03 15:12:38 +0400

    Enhance MemberRepository with CRUD operations

--- Repositories/MemberRepository.cs
+++ Repositories/MemberRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleLibraryManagement_LayeredArchitectureAndRepository.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,44 @@ namespace SimpleLibraryManagement_LayeredArchitectureAndRepository.Repositories
 {
     class MemberRepository
     {
-    }
+        public void AddMember(Member member)
+        {
+            var members = GetAllMembers();
+            members.Add(member);
+            FileContext.SaveMember(members);
+        }
+
+        public Member GetMember(int id)
+        {
+            return GetAllMembers().FirstOrDefault(m => m.Id == id);
+        }
+
+        public List<Member> GetAllMembers()
+        {
+            return FileContext.LoadMember();
+        }
+
+        public void UpdateMember(Member member)
+        {
+            var members = GetAllMembers();
+            var index = members.FindIndex(m => m.Id == member.Id);
+            if (index != -1)
+            {
+                members[index] = member;
+                FileContext.SaveMember(members);
+            }
+        }
+
+        public void DeleteMember(int id)
+        {
+            var members = GetAllMembers();
+            var memberToRemove = members.FirstOrDefault(m => m.Id == id);
+            if (memberToRemove != null)
+            {
+                members.Remove(memberToRemove);
+                FileContext.SaveMember(members);
+            }
+        }
+
+        }
 }
