463ac1df5eab058cbea9f4c598db987373e18926
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-03 14:36:42 +0400

    Add properties and navigation for BorrowRecord class
    Updated the `BorrowRecord` class to include properties: `Id`, `MemberId`, `BookId`, `BorrowDate`, and a nullable `ReturnDate`. Added navigation properties for `Member` and `Book` to establish relationships with these entities.

--- Models/BorrowRecord.cs
+++ Models/BorrowRecord.cs
@@ -8,5 +8,16 @@ namespace SimpleLibraryManagement_LayeredArchitectureAndRepository.Models
 {
     class BorrowRecord
     {
+        // Properties Id, MemberId, BookId, BorrowDate, ReturnDate
+        public int Id { get; set; }
+        public int MemberId { get; set; }
+        public int BookId { get; set; }
+        public DateTime BorrowDate { get; set; }
+        public DateTime? ReturnDate { get; set; } // Nullable to allow for books that haven't been returned yet
+        
+        // Navigation properties
+        public Member Member { get; set; }
+        public Book Book { get; set; }
+
     }
 }
