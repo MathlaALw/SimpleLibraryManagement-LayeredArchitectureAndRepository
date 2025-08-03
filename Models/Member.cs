349c655b89ba87c989d05e00f42b5921c189819b
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-03 14:35:03 +0400

    Add Id and Name properties to Member class
    This commit introduces two new properties, `Id` and `Name`, to the `Member` class in the `SimpleLibraryManagement_LayeredArchitectureAndRepository.Models` namespace. These additions enhance the class by providing a unique identifier and a name for each member.

--- Models/Member.cs
+++ Models/Member.cs
@@ -8,5 +8,8 @@ namespace SimpleLibraryManagement_LayeredArchitectureAndRepository.Models
 {
     class Member
     {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
