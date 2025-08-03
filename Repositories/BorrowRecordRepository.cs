cd44cb1bc7da23b9d6a746310cebcb8f547c20bc
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-03 15:09:13 +0400

    Add BorrowRecordRepository for managing borrow records

--- Repositories/BorrowRecordRepository.cs
+++ Repositories/BorrowRecordRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleLibraryManagement_LayeredArchitectureAndRepository.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,43 @@ namespace SimpleLibraryManagement_LayeredArchitectureAndRepository.Repositories
 {
     class BorrowRecordRepository
     {
+       
+        public void AddBorrowRecord(BorrowRecord record)
+        {
+            
+        }
+
+        public BorrowRecord GetBorrowRecord(int id)
+        {
+            return GetAllBorrowRecords().FirstOrDefault(r => r.Id == id);
+        }
+
+        public List<BorrowRecord> GetAllBorrowRecords()
+        {
+            return FileContext.LoadBorrowRecord();
+        }
+
+        public void UpdateBorrowRecord(BorrowRecord record)
+        {
+            var records = GetAllBorrowRecords();
+            var index = records.FindIndex(r => r.Id == record.Id);
+            if (index != -1)
+            {
+                records[index] = record;
+                FileContext.SaveBorrowRecord(records);
+            }
+        }
+
+        public void DeleteBorrowRecord(int id)
+        {
+            var records = GetAllBorrowRecords();
+            var recordToRemove = records.FirstOrDefault(r => r.Id == id);
+            if (recordToRemove != null)
+            {
+                records.Remove(recordToRemove);
+                FileContext.SaveBorrowRecord(records);
+            }
+        }
 
     }
 }
