bd59e5fe74d00f122fc0dbdf45323b8c57d216c1
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-03 16:00:11 +0400

    Refactor borrowing and returning book methods

--- Services/LibraryService.cs
+++ Services/LibraryService.cs
@@ -21,25 +21,82 @@ namespace SimpleLibraryManagement_LayeredArchitectureAndRepository.Services
             _memberRepository = memberRepository;
             _borrowRecordRepository = borrowRecordRepository;
         }
-        public void BorrowBook(string bookId, string memberId)
-        { 
+        public void BorrowBook(int bookId, int memberId)
+        {
+
+            var book = _bookRepository.GetBook(bookId);
+            var member = _memberRepository.GetMember(memberId);
+            if (book == null)
+            {
+                Console.WriteLine("Book not found.");
+            }
+            if (member == null)
+            {
+                Console.WriteLine("Member not found.");
+            }
+            if (!book.IsAvailable)
+            {
+                Console.WriteLine("Book is not available for borrowing.");
+            }
+            book.IsAvailable = false;
+            _bookRepository.UpdateBook(book);
+            var borrowRecord = new BorrowRecord
+            {
+                BookId = book.Id,
+                MemberId = member.Id,
+                BorrowDate = DateTime.Now
+            };
+            _borrowRecordRepository.AddBorrowRecord(borrowRecord);
 
         }
 
-        public void ReturnBook(string bookId, string memberId)
+        public void ReturnBook(int bookId, int memberId)
         {
+            var book = _bookRepository.GetBook(bookId);
+            var member = _memberRepository.GetMember(memberId);
+            if (book == null)
+            {
+                Console.WriteLine("Book not found.");
+                return;
+            }
+            if (member == null)
+            {
+                Console.WriteLine("Member not found.");
+                return;
+            }
+            if (book.IsAvailable)
+            {
+                Console.WriteLine("Book is already available.");
+                return;
+            }
+            book.IsAvailable = true;
+            _bookRepository.UpdateBook(book);
+            _borrowRecordRepository.DeleteBorrowRecord(bookId, memberId);
 
         }
 
         public void RegisterMember(Member member)
         {
+            if (member == null)
+            {
+                Console.WriteLine("Member cannot be null.");
+                return;
+            }
+            _memberRepository.AddMember(member);
+            Console.WriteLine("Member registered successfully.");
 
         }
 
 
         public void AddBook(Book book)
         {
-
+            if (book == null)
+            {
+                Console.WriteLine("Book cannot be null.");
+                return;
+            }
+            _bookRepository.AddBook(book);
+            Console.WriteLine("Book added successfully.");
 
         }
 
