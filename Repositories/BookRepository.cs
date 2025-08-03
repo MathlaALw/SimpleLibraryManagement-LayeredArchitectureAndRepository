d50c27cdf4b2c901a27c2feb48e05209ea599f33
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-03 14:54:57 +0400

    Add book management methods to BookRepository

--- Repositories/BookRepository.cs
+++ Repositories/BookRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleLibraryManagement_LayeredArchitectureAndRepository.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,38 @@ namespace SimpleLibraryManagement_LayeredArchitectureAndRepository.Repositories
 {
     class BookRepository
     {
+
+        public void AddBook(Book book)
+        {
+            // Logic to add a book to the repository
+            // This could involve adding the book to a database or an in-memory collection
+        }
+
+        public Book GetBook(int id)
+        {
+            // Logic to retrieve a book by its ID
+            // This could involve querying a database or an in-memory collection
+            return new Book(); // Placeholder return statement
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            // Logic to retrieve all books
+            // This could involve querying a database or returning an in-memory collection
+            return new List<Book>(); // Placeholder return statement
+        }
+
+        public void UpdateBook(Book book)
+        {
+            // Logic to update a book in the repository
+            // This could involve updating the book in a database or an in-memory collection
+        }
+
+        public void DeleteBook(int id)
+        {
+            // Logic to delete a book by its ID
+            // This could involve removing the book from a database or an in-memory collection
+        }
+
     }
 }
